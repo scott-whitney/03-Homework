ea39c14c8bc70b3f5d8fda0f363a8ee44a0a331e
Minroe <scott.whitney334@gmail.com>
2020-03-18 09:48:41 -0700

    Fixed Button Prompt

--- script02.js
+++ script02.js
@@ -4,8 +4,12 @@ generateButton.addEventListener('click', writePassword)
 
 
 function writePassword() {
-    
-    var passwordNow = newPass.join("");
+
+
+
+    var passwordNow = genPass();
+    // var passwordNew = passwordNow.join("");
+    // var passwordNow = newPass.join("");
     var passwordText = document.querySelector("#password");
 
     // var x = document.getElementById("#password")
@@ -14,68 +18,75 @@ function writePassword() {
     passwordText.value = passwordNow;
 } 
 
-var newPass = [];
-var smalls = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m",];
-var Large = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M",];
-var specialChar = ["#","!","@","$","%"]
-var nums = ["1","2","3","4","5","6","7","8","9","0"]
+function genPass() {
 
-passLength = prompt("pick a # between 8 and 128 please")
 
-var selectedChoices = 4;
+    var newPass = [];
+    var smalls = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m",];
+    var Large = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M",];
+    var specialChar = ["#","!","@","$","%"]
+    var nums = ["1","2","3","4","5","6","7","8","9","0"]
+
+    passLength = prompt("pick a # between 8 and 128 please")
+
+    var selectedChoices = 4;
 // var trueLength = passLength / selectedChoices;
 
-console.log(selectedChoices);
+    console.log(selectedChoices);
 
 
-var doSmalls = confirm("Do you want lowercase letters in your Password?")
-var doLarge = confirm("Do you want uppercase letters in your new password?")
-var doSpecialChar = confirm("Do you want special characters in your new password?")
-var doNums = confirm("Do you want numbers in your new password?") 
+    var doSmalls = confirm("Do you want lowercase letters in your Password?")
+    var doLarge = confirm("Do you want uppercase letters in your new password?")
+    var doSpecialChar = confirm("Do you want special characters in your new password?")
+    var doNums = confirm("Do you want numbers in your new password?") 
 
-function passGen(arr) {
+    function passGen(arr) {
 
-    for (var i = 0; i < trueLength; i++) {
+        for (var i = 0; i < trueLength; i++) {
         var numnum = Math.floor(Math.random() * arr.length); 
         newPass.push(arr[numnum])
 
+        }
     }
-}
 
-if (!doSmalls) {
+    if (!doSmalls) {
     // passGen(smalls);
     selectedChoices--
-}
-if (!doLarge) {
+    }
+    if (!doLarge) {
     // passGen(Large);
     selectedChoices--
-}
-if (!doSpecialChar) {
+    }
+    if (!doSpecialChar) {
     // passGen(specialChar);
     selectedChoices--
-}
-if (!doNums) {
+    }
+    if (!doNums) {
     // passGen(nums);
     selectedChoices--
-}
+    }
 
-var trueLength = passLength / selectedChoices;
+    var trueLength = passLength / selectedChoices;
 
 
-if (doSmalls) {
+    if (doSmalls) {
     passGen(smalls); 
-}
-if (doLarge) {
+    }
+    if (doLarge) {
     passGen(Large);
-}
-if (doSpecialChar) {
+    }
+    if (doSpecialChar) {
     passGen(specialChar);
-}
-if (doNums) {
+    }
+    if (doNums) {
     passGen(nums);
+    }
+
+    
+    oldNewPass = newPass.join("");
+    
+    return oldNewPass;
 }
-
-
 
 
 
